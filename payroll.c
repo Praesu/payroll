0d2fd3d88940f5ae1386df72eab27eb12aba2796
luke <colinalukesc@gmail.com>
2021-02-24 15:15:53 +0800

    flow

--- payroll.c
+++ payroll.c
@@ -2,7 +2,17 @@
 
 int main()
 {
-    printf("Hello world");
+    int a;
+    
+    printf("1. View Employee List\n");
+    printf("2. Calculate Payroll\n");
+    printf("3. End of day inputs\n");
+    printf("4. Settings\n");
+    printf("5. Exit\n");
+
+    printf("What do you want to do? Enter number: ");
+    scanf("%d", &a);
+
 
     return 0;
 }
